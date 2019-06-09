d363fedf7420280a54e056e74e79d90c8d0ab1e7
Priyam Vora <priyamvora@Priyams-MacBook-Pro.local>
2019-06-09 16:23:53 +0530

    Heroku integrations and changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 var app=express();
-
+const port=process.env.PORT||3000;
 hbs.registerPartials(__dirname+'/views/partials');
 
 app.set('view engine','hbs');
@@ -53,4 +53,4 @@ app.get("/bad.html",(req,res)=>{
     error:"File not found!"
   });
 });
-app.listen(3000);
+app.listen(port);
